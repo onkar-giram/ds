59965875f64ed46af798a8ee032555c08a92decb
Onkar Giram <77536983+onkar-giram@users.noreply.github.com>
2023-05-24 17:10:44 +0530

    first commit

--- A1/ServerIntf.java
+++ A1/ServerIntf.java
@@ -2,7 +2,6 @@ import java.rmi.*;
 
 
 interface ServerIntf extends Remote{
-	// Syntax for method declaration: access_specifier return_type method_name(arguments...){ return value}
 	
 	public double Addition(double num1, double num2) throws RemoteException;
 	public double Subtraction(double num1, double num2) throws RemoteException;
